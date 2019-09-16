2036c40a2dc3c50e120777a35af09a7519fe1501
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-09-16 09:13:17 -0500

    first commit

---
+++ algorithmOne.js
@@ -0,0 +1,28 @@
+// function pythagorean(a,b) {
+//     return a*a + b*b
+// }
+//
+// console.log(pythagorean(5,12))
+//
+// function isItPythag(a,b) {
+//     return Math.sqrt(pythagorean(a,b))
+// }
+//
+// console.log(isItPythag(5,12))
+
+// string1 = "apples"
+// string2 = "pineapples"
+//
+// function compare() {
+//   let arr1 = string1.split("")
+//   let arr2 = string2.split("")
+//   const finalArr = []
+//   arr1.forEach((e1) => arr2.forEach((e2) => {
+//     if(e1===e2){
+//       finalArr.push(e1)
+//     }
+//   }))
+//   return finalArr
+// }
+//
+// console.log(compare())
