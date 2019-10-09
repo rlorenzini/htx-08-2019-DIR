9430d512dbfb9780bffd5edd312a8cf1611a4d17
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-10-09 10:20:54 -0500

    pug app practice

---
+++ w7backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+  app = express(),
+  port = process.env.PORT || 8080;
+
+app.use(express.static("styles"));
+//sets a static import to the styles folder for your .pug files to use
+
+app.set("view engine", "pug"); //do not need to require("pug")
+
+app.get('/', (req,res) => {
+  console.log("hello")
+  res.render("index", { title: "pugapp", message: "hello there!" });
+});
+//first argument "index" checks the VIEWS directory for an index template
+//second argument can take an object which can pass data into the template
+
+app.get("/about", (req,res) => {
+  res.render("about")
+})
+
+app.listen(port, () => {console.log(`listening on ${port}`)});
