0d539de1fa7a726cf8c8a144f62e346693cf8165
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-11-05 09:55:02 -0600

    more comments

--- react-book-website-with-routers/src/components/BaseLayout.js
+++ react-book-website-with-routers/src/components/BaseLayout.js
@@ -5,6 +5,11 @@ import Footer from './Footer';
 
 //BaseLayout is passing the history to Header and the children to Content
 
+//WHY CLASS COMPONENT VS FUNCTION?
+//WE ARE USING STATE, SO WE MUST USE A COMPONENT!!!!!!!!
+//History, children, this, props... it cannot be used/accessed/passed
+//in a stateless component (aka function)
+
 class BaseLayout extends Component {
   render(){
     return (
--- react-book-website-with-routers/src/components/NavLinks/Home.js
+++ react-book-website-with-routers/src/components/NavLinks/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div>
-This is the home page
+      This is the home page
     </div>
   );
 }
