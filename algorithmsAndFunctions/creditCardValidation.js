705be690fe0d4afbd1c35fa8a38890473c2fe3c5
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-11-15 10:48:09 -0600

    added regex code

--- algorithmsAndFunctions/creditCardValidation.js
+++ algorithmsAndFunctions/creditCardValidation.js
@@ -46,3 +46,15 @@ console.log(CardChecker(discoverCard));
 console.log(CardChecker(masterCard));
 console.log(CardChecker(visaCard));
 console.log(CardChecker(americanExpress));
+
+
+//normally youd check with a regular expression (RegEx)
+//this is a demonstration of using basic algorithms and concepts to create
+//functions for real life applications
+
+
+// this regex that matches Visa, MasterCard, American Express, Diners Club, Discover, and JCB cards:
+
+// ^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$
+
+//quicker, easier, and faster than a nested conditional statement 
