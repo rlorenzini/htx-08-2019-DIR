58188ce0293f4038b866e161f2190779c3a0402d
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-12-20 14:20:00 -0600

    stuff

--- algorithmsAndFunctions/dataManipulation.js
+++ algorithmsAndFunctions/dataManipulation.js
@@ -1,31 +1,84 @@
-// HARD
-// given a json object, remove the beginning and end and return a new object
-// GloryToPotatoes({ key:value })
-// array = GloryToPotatoes.split('') => splice = array.indexOf('('); pop = array.lastIndexOf(')');
-// newArray.splice(0, splice + 1); string = newArray.join(''); JSON.parse(string)
+// SORT ARRAY
+let array = [1,5,6,87,6,8,90,3]
+console.log(array)
+// let newArray = []
+// console.log(newArray[7])
+// console.log(newArray.length)
+for (let i = 0; i < array.length - 1; i++) {
+  for (let j = i; j < array.length; j++) {
+    if (array[j] > array[i]) {
+      let t = array[i]
+      array[i] = array[j]
+      array[j] = t
+    }
+  }
+}
 
-// EASY
-// sort an arry [1,2,3,4,5,6] = [6,5,4,3,2,1]
-// sort((a,b) => return b - a)
+// console.log(array)
+// COMBINE ARRAYS
+let arr1 = [1,2,3,4]
+let arr2 = [1,2,3]
+let arr3 = [4,6,7,8]
 
-// MEDIUM
-// give two index values, find all properties between them
-// 5, 10 array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14] = [6,7,8,9,10,11]
-// array.slice(array.indexOf(5), array.indexOf(6) + 1)
-// slice(index to start at, index to end at but not include)
+let mergeArray = []
 
-// EASY
-// {{ key,value },{ key:value }} add a new object
-// myOBjectOFObjects = Object.assign({ key: value })
+// for (let i = 0; i < arr1.length; i++) {
+//   mergeArray.push(arr1[i])
+// }
+// for (let i = 0; i < arr2.length; i++) {
+//   mergeArray.push(arr2[i])
+// }
+// for (let i = 0; i < arr3.length; i++) {
+//   mergeArray.push(arr3[i])
+// }
 
-// MEDIUM
-// remove all objects that do not match the given key
-// stuff = { brand: potato, color: blue, word: yes, no: maybe }; prop = no
-// newStuff = Object.keys(car).reduce((object, key) => {
-//    if (key !== prop) { object[key] = stuff[key] }
-// return object
-//})
+let merged = arr1.concat(arr2,arr3)
+// console.log(arr1)
+// console.log(merged)
+// GET BETWEEN TWO VALUES OF ARRAY
+let bigArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+let first = 3
+let second = 7
 
-// EASY
-// merge three arrays
-// arr1.concat(arr2, arr3)
+let newBigArray = []
+
+for (let i = first; i <= second; i++) {
+  newBigArray.push(bigArray[i])
+}
+
+// console.log(newBigArray)
+// ADD OBJECT TO OBJECT
+let objects = { 1:{key: "value"}, 2:{key: "value"} }
+let obj = {name: "potatoes", value: "delicious", type: "all", glory: "To Potatoes"}
+
+objects.new = obj
+// console.log(objects)
+// REMOVE OBJECT VALUE
+console.log(obj)
+// Object.keys
+delete obj.name
+console.log(obj)
+
+let potatoes = Object.keys(obj)
+let glory = "glory"
+console.log(potatoes)
+
+console.log(potatoes.splice(0, potatoes.indexOf(glory)))
+
+// CRAZY THING
+
+let crazy = 'GloryToPotatoes({"key":"value"})'
+
+let crazyArray = crazy.split('')
+
+let splicedDigit = crazyArray.indexOf('(')
+let poppedDigit = crazyArray.lastIndexOf(')')
+
+crazyArray.splice(0, splicedDigit + 1)
+crazyArray.pop(poppedDigit)
+
+let notCrazyArray = crazyArray.join('')
+
+let finalObject = JSON.parse(notCrazyArray)
+
+console.log(finalObject)
